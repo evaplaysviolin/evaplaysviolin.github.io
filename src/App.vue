0477b74dfca168f63553a2c93521f8a228c994ce
Eva <evaplaysviolin@gmail.com>
2019-09-09 21:42:43 -0700

    Add font awesome, node garden

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 
 <div id="app">
 
+  <div id="node-garden-container"></div>
+  <!-- <svg class="moon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+    <path d="M86.576,84.239c-0.102-0.44-0.448-0.781-0.889-0.876c-1.441-0.311-2.879-0.689-4.271-1.127  C66.969,77.707,55.8,66.938,50.77,52.692C45.739,38.445,47.67,23.05,56.07,10.454c0.812-1.218,1.692-2.415,2.617-3.559  c0.284-0.351,0.339-0.834,0.143-1.24C58.632,5.25,58.219,4.994,57.768,5c-4.879,0.064-9.708,0.928-14.354,2.568  c-23.396,8.261-35.71,34.016-27.449,57.412c8.261,23.397,34.017,35.711,57.414,27.45c4.645-1.64,8.945-3.999,12.783-7.014  C86.516,85.138,86.678,84.679,86.576,84.239z">
+    </path>
+  </svg> -->
+  <div id="moon-container">
+    <font-awesome-icon :icon="['fas', 'moon']" />
+  </div>
+
   <div id="page-container">
 
-    <div id="header-container">
-      asdf
-    </div>
+    <div id="header-container">header?</div>
 
     <!-- Set up "views" for components -->
     <!-- Empty view is where main components for other pages is inserted -->
@@ -18,8 +25,7 @@
     <router-view name="featured-partners"></router-view>
     <router-view name="featured-galleries"></router-view> -->
 
-    <div id="footer-container">
-    </div>
+    <div id="footer-container">footer?</div>
   
   </div>
 
@@ -30,6 +36,9 @@
 
 
 <script>
+
+import NodeGarden from './vendors/nodegarden.js';
+
 
 // import media from "./mixins/media.js";
 export default {
@@ -46,6 +55,49 @@ export default {
   methods: {
   },
   mounted() {
+    const pixelRatio = window.devicePixelRatio;
+    const $container = document.getElementById('node-garden-container');
+    const $moon = document.getElementById('moon-container');
+
+    const nodeGarden = new NodeGarden($container);
+
+    // start simulation
+    nodeGarden.start();
+
+    // trigger nightMode automatically
+    const date = new Date();
+
+    if (date.getHours() > 18 || date.getHours() < 6) {
+      nodeGarden.toggleNightMode();
+    }
+
+    let resetNode = 0;
+
+    $container.addEventListener('click', (e) => {
+      const bcr = $container.getBoundingClientRect();
+      const scrollPos = {
+        x: window.scrollX,
+        y: window.scrollY
+      };
+      resetNode++;
+      if (resetNode > nodeGarden.nodes.length - 1) {
+        resetNode = 1;
+      }
+      nodeGarden.nodes[resetNode].reset({
+        x: (e.pageX - scrollPos.x - bcr.left) * pixelRatio,
+        y: (e.pageY - scrollPos.y - bcr.top) * pixelRatio,
+        vx: 0,
+        vy: 0
+      });
+    });
+
+    $moon.addEventListener('click', () => {
+      nodeGarden.toggleNightMode();
+    });
+
+    window.addEventListener('resize', () => {
+      nodeGarden.resize();
+    });
   }
 }
 
@@ -70,11 +122,54 @@ export default {
   box-sizing: border-box;
 }
 
-// body {
-//   @include flex-center;
-//   align-items: flex-start;
-//   background-color: $lightest-blue;
-// }
+body {
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+#node-garden-container {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+}
+
+body {
+  background-color: #f5f5f5;
+  overflow: hidden;
+  transition: background-color 0.2s ease-in-out;
+}
+  body.nightmode {
+    background-color: #0a0a0a;
+  }
+  body.nightmode #moon-container {
+    color: #fff;
+  }
+  body.nightmode #moon-container:hover {
+    background-color: #fefefe;
+    color: #000;
+  }
+
+#moon-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 45px;
+  height: 45px;
+  padding: 1rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition:background-color 0.2s ease-in-out;
+}
+#moon-container:hover {
+  background-color:#0a0a0a;color:#fff
+}
+
+
 
 // #page-container {
 //   @include flex-center;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import router from "./route.js";
 
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// config.autoAddCss = false;
+library.add(faMoon)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false;
 
 new Vue({
