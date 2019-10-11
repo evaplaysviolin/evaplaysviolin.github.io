d3531e4551c316ee62d9230deef4ed8d7dae32e0
Eva <evaplaysviolin@gmail.com>
2019-10-11 15:34:03 -0700

    Change color of header dividing line (switch day and night mode)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,21 +29,36 @@ export default {
   top: 0;
   // border-bottom: 2px solid rgba(0, 0, 0, 0.1);
   // background-color: rgba(0, 0, 0, 0.1);
-  // Fallback style for border-image
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+  // // Fallback style for border-image
+  // border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+  // // border-style: solid;
+  // border-image: linear-gradient(
+  //   90deg, 
+  //   $gradient-red, 
+  //   $gradient-blue,
+  //   $gradient-green
+  //   ) 0 0 100% 0 / 2px;
+
+  border-image: none;
+  border-bottom: 2px solid rgb(100, 100, 100);
+}
+  body.nightmode #header-container {
+    // border-bottom: 2px solid rgba(255, 255, 255, 0.1);
+    // background-color: rgba(255, 255, 255, 0.1);
+    
+    // border-image: none;
+    // border-bottom: 2px solid white;
+
+    // Fallback style for border-image
+  border-bottom: 2px solid white;
   // border-style: solid;
   border-image: linear-gradient(
     90deg, 
     $gradient-red, 
     $gradient-blue,
     $gradient-green
-    ) 0 0 100% 0 / 1px;
-}
-  body.nightmode #header-container {
-    // border-bottom: 2px solid rgba(255, 255, 255, 0.1);
-    // background-color: rgba(255, 255, 255, 0.1);
-    border-image: none;
-    border-bottom: 1px solid white;
+    ) 0 0 100% 0 / 2px;
   }
 
 </style>
