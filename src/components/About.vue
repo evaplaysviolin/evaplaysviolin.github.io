e9edace159d6a5cac1f5a73c9f1672383a8f956d
Eva <evaplaysviolin@gmail.com>
2019-10-12 18:25:06 -0700

    Empty template for About page

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div id="about-container"></div>
+
+</template>
+
+
+
+<script>
+
+export default { 
+  name: "About",
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+
+
+
+<style scoped lang="scss">
+
+// #about-container {
+// }
+  // body.nightmode #about-container {
+  // }
+
+</style>
