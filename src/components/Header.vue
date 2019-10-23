e494c0b8bd61cf8479336174198a3411a6b6b9f0
Eva <evaplaysviolin@gmail.com>
2019-10-23 15:29:28 -0700

    Add comments, rename main function

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
 
 <div id="header-container" ref="headerContainer">
+  <!-- Canvas with a gradient line pretending to be border-bottom for night mode -->
   <canvas id="header-border" ref="headerBorder" v-show="night"></canvas>
   <router-link :to="{ path: '/' }">home</router-link>
 </div>
@@ -20,21 +21,17 @@ export default {
   },
   watch: {
     night () {
-      this.test();
+      this.createHeaderCanvas();
     }
   },
   methods: {
-    test() {
+    createHeaderCanvas() {
       let canvas = this.$refs.headerBorder;
       let ctx = canvas.getContext("2d");
       let container = this.$refs.headerContainer;
       let measurements = container.getBoundingClientRect();
-      // console.log(measurements.width);
-      // console.log(measurements.height);
       canvas.width = measurements.width;
-      // console.log(canvas.width);
       canvas.height = measurements.height;
-      // console.log(canvas.height);
       let w = canvas.width;
       let h = canvas.height;
       let lineGradient = ctx.createLinearGradient(0, 0, w, 0);
@@ -45,23 +42,17 @@ export default {
       ctx.clearRect(0, 0, w, h);
       ctx.beginPath();
       ctx.lineWidth = "2";
-      // ctx.translate(0.5, 0.5);
-      // ctx.moveTo(0, h);
-      // ctx.lineTo(w, h);
+      // h - 1 to create a 2px line
       ctx.moveTo(0, h - 1);
       ctx.lineTo(w, h - 1);
-      // ctx.moveTo(0, h - 0.5);
-      // ctx.lineTo(w, h - 0.5);
-      // ctx.moveTo(0, h);
-      // ctx.lineTo(w, h);
       ctx.stroke();
     }
   },
   mounted() {
-    this.test();
+    this.createHeaderCanvas();
 
     window.addEventListener("resize", () => {
-      this.test();
+      this.createHeaderCanvas();
     });
   }
 }
@@ -73,58 +64,24 @@ export default {
 <style scoped lang="scss">
 
 #header-container {
+  @include flex-center;
   height: 75px;
   width: 100%;
   position: fixed;
   top: 0;
-  @include flex-center;
-  // border-bottom: 2px solid rgba(0, 0, 0, 0.1);
-  // background-color: rgba(0, 0, 0, 0.1);
-
-  // // Fallback style for border-image
-  // border-bottom: 2px solid rgba(0, 0, 0, 0.1);
-  // // border-style: solid;
-  // border-image: linear-gradient(
-  //   90deg, 
-  //   $gradient-red, 
-  //   $gradient-blue,
-  //   $gradient-green
-  //   ) 0 0 100% 0 / 2px;
-
-  border-image: none;
-  // border-bottom: 2px solid rgb(100, 100, 100);
   border-bottom: 2px solid black;
 }
   body.nightmode #header-container {
-    // border-bottom: 2px solid rgba(255, 255, 255, 0.1);
-    // background-color: rgba(255, 255, 255, 0.1);
-    
-    // border-image: none;
-    // border-bottom: 2px solid white;
-
-    // Fallback style for border-image
-  // border-bottom: 2px solid white;
-  border-bottom: 2px solid transparent;
-  // border-style: solid;
-  // border-image: linear-gradient(
-  //   90deg, 
-  //   $gradient-red, 
-  //   $gradient-blue,
-  //   $gradient-green
-  //   ) 0 0 100% 0 / 2px;
-  // border-bottom: none;
+    // Canvas "border" sits on top of this
+    // Extra border used due to box-sizing: border-box
+    border-bottom: 2px solid transparent;
   }
 
   #header-border {
-    // height: calc(100% + 2px);
-    // height: 100%;
-    // width: 100%;
     position: absolute;
     top: 0;
-  }
-
-  #header-container a {
-    z-index: 75;
+    // No pointer events for canvas that overlays everything
+    pointer-events: none;
   }
 
 </style>
