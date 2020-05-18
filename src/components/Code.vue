3574f6089034d54deb93bb115f0a95771f28d3d1
Eva <evaplaysviolin@gmail.com>
2020-05-18 12:02:35 -0700

    Add hover and cursor changes, style placeholder text

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -4,26 +4,20 @@
   <div id="examples-container" :style="detailsOpen ? pin : {}">
     <div class="example"
       v-for="(example, index) in examples" :key="example">
-      <div class="large image-container" @click="openDetails(index)">
+      <div class="large image-container" 
+        :class="{ 'large-shadow': !detailsOpen }"
+        @click="openDetails(index)">
+        <div class="large-mouseover"
+          @mouseover="exampleHover('over', index)"
+          @mouseleave="exampleHover('leave', index)">
+          <div class="mouseover-text"
+            v-if="hover === index">
+            {{ titles[index] }}
+          </div>
+        </div>
         <div class="fade" v-if="detailsOpen"></div>
         <img :src="image(example, 'large')" />
       </div>
-      <!-- <div class="example-detail" v-if="current === index">
-        <div class="detail-close" 
-          :style="position"
-          @click="closeDetails()">
-          <font-awesome-icon :icon="['fas', 'times']" />
-        </div>
-        <div class="large-detail image-container"><img :src="image(example, 'large')" /></div>
-        <div class="small-container">
-          <div class="small image-container"><img :src="image(example, '1')" /></div>
-          <div class="small image-container"><img :src="image(example, '2')" /></div>
-          <div class="small image-container"><img :src="image(example, '3')" /></div>
-        </div>
-        <div class="detail-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem at nisi feugiat luctus. Quisque mollis elit lorem, facilisis vehicula leo rutrum non. Quisque ullamcorper pharetra orci, quis venenatis magna dictum sed. In pharetra orci eros, sit amet fringilla dui interdum id. Proin feugiat diam id iaculis pretium. Morbi ac ex eu turpis tincidunt bibendum ullamcorper ut nibh. Nunc vitae consequat massa. Suspendisse vitae est nec mi maximus maximus. Vivamus pretium, orci vitae tristique aliquam, sapien nisl efficitur orci, et ullamcorper sapien mauris id justo. Nulla pulvinar porta nulla sed feugiat. Pellentesque mollis et leo eget viverra. Nunc interdum purus tellus, at porta velit ullamcorper nec. Curabitur porta sem placerat mi tincidunt consequat. Mauris sagittis imperdiet lectus sed mattis. In ut libero et turpis facilisis ultrices a sit amet orci. Nunc pulvinar scelerisque eleifend. Donec feugiat augue sed justo auctor faucibus. Nam ut nisl non est cursus pharetra. Curabitur vestibulum magna eget lectus egestas tempus. Vestibulum massa justo, facilisis id nulla at, eleifend auctor risus. Ut id nibh sem. Phasellus at ullamcorper orci. Cras ac diam massa. Nullam scelerisque ex eu fermentum elementum. Vivamus a urna non tortor molestie vulputate. In nec justo quis urna convallis volutpat. Quisque laoreet eros eu pulvinar posuere. Mauris ut venenatis magna, et commodo nisl. Proin purus nulla, gravida a consequat eget, vestibulum sed enim. Nunc vitae arcu libero. Mauris a tortor felis. Sed sed erat quis lorem congue ornare vitae in dui. In cursus nec magna eget gravida. Maecenas finibus feugiat magna quis lobortis. Suspendisse lacinia quam vel dolor molestie vulputate. Nam ut vulputate massa. Aenean maximus erat odio. Duis lacinia ac est maximus sodales. Curabitur risus risus, commodo nec euismod in, varius ac augue. Vivamus tempor dignissim augue vitae aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eget iaculis lorem, fringilla luctus neque. Nunc quam tortor, vestibulum quis libero ac, consectetur vestibulum tellus. Curabitur imperdiet nisi diam, vitae mattis purus porttitor quis. In sed sem ligula. Vivamus fringilla placerat nisi, in efficitur diam viverra in. Praesent elit quam, consectetur quis est quis, congue mattis lorem. Aenean tempor, eros sit amet semper viverra, tellus quam posuere mauris, eget condimentum nisl elit id quam. Donec sed convallis purus, vel suscipit sem. Proin porta mauris ac eleifend elementum. Proin tellus leo, semper consectetur odio sed, molestie ultricies sem. Vivamus condimentum, purus fringilla mattis euismod, mi lectus mollis metus, at posuere augue libero et erat. Morbi laoreet ultricies nulla, id sodales nisi feugiat vel. Nam sit amet eleifend felis. Aenean eleifend velit sem, et sagittis erat accumsan et. Nulla quis dolor rutrum, finibus arcu eget, ornare nisl. Integer ultrices dictum nibh vel sodales. In facilisis dolor eu condimentum congue. Nullam sed maximus nibh. Vivamus interdum dolor sed leo molestie, sed maximus enim tristique. Morbi imperdiet eget massa quis lobortis. Nullam mollis finibus urna, a auctor massa rhoncus ut. Suspendisse orci quam, congue nec nisl at, eleifend porta libero.
-        </div>
-      </div> -->
     </div>
   </div>
   <div id="details-container">
@@ -35,14 +29,16 @@
           @click="closeDetails()">
           <font-awesome-icon :icon="['fas', 'times']" />
         </div>
-        <div class="large-detail image-container"><img :src="image(example, 'large')" /></div>
-        <div class="small-container">
-          <div class="small image-container"><img :src="image(example, '1')" /></div>
-          <div class="small image-container"><img :src="image(example, '2')" /></div>
-          <div class="small image-container"><img :src="image(example, '3')" /></div>
-        </div>
-        <div class="detail-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem at nisi feugiat luctus. Quisque mollis elit lorem, facilisis vehicula leo rutrum non. Quisque ullamcorper pharetra orci, quis venenatis magna dictum sed. In pharetra orci eros, sit amet fringilla dui interdum id. Proin feugiat diam id iaculis pretium. Morbi ac ex eu turpis tincidunt bibendum ullamcorper ut nibh. Nunc vitae consequat massa. Suspendisse vitae est nec mi maximus maximus. Vivamus pretium, orci vitae tristique aliquam, sapien nisl efficitur orci, et ullamcorper sapien mauris id justo. Nulla pulvinar porta nulla sed feugiat. Pellentesque mollis et leo eget viverra. Nunc interdum purus tellus, at porta velit ullamcorper nec. Curabitur porta sem placerat mi tincidunt consequat. Mauris sagittis imperdiet lectus sed mattis. In ut libero et turpis facilisis ultrices a sit amet orci. Nunc pulvinar scelerisque eleifend. Donec feugiat augue sed justo auctor faucibus. Nam ut nisl non est cursus pharetra. Curabitur vestibulum magna eget lectus egestas tempus. Vestibulum massa justo, facilisis id nulla at, eleifend auctor risus. Ut id nibh sem. Phasellus at ullamcorper orci. Cras ac diam massa. Nullam scelerisque ex eu fermentum elementum. Vivamus a urna non tortor molestie vulputate. In nec justo quis urna convallis volutpat. Quisque laoreet eros eu pulvinar posuere. Mauris ut venenatis magna, et commodo nisl. Proin purus nulla, gravida a consequat eget, vestibulum sed enim. Nunc vitae arcu libero. Mauris a tortor felis. Sed sed erat quis lorem congue ornare vitae in dui. In cursus nec magna eget gravida. Maecenas finibus feugiat magna quis lobortis. Suspendisse lacinia quam vel dolor molestie vulputate. Nam ut vulputate massa. Aenean maximus erat odio. Duis lacinia ac est maximus sodales. Curabitur risus risus, commodo nec euismod in, varius ac augue. Vivamus tempor dignissim augue vitae aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eget iaculis lorem, fringilla luctus neque. Nunc quam tortor, vestibulum quis libero ac, consectetur vestibulum tellus. Curabitur imperdiet nisi diam, vitae mattis purus porttitor quis. In sed sem ligula. Vivamus fringilla placerat nisi, in efficitur diam viverra in. Praesent elit quam, consectetur quis est quis, congue mattis lorem. Aenean tempor, eros sit amet semper viverra, tellus quam posuere mauris, eget condimentum nisl elit id quam. Donec sed convallis purus, vel suscipit sem. Proin porta mauris ac eleifend elementum. Proin tellus leo, semper consectetur odio sed, molestie ultricies sem. Vivamus condimentum, purus fringilla mattis euismod, mi lectus mollis metus, at posuere augue libero et erat. Morbi laoreet ultricies nulla, id sodales nisi feugiat vel. Nam sit amet eleifend felis. Aenean eleifend velit sem, et sagittis erat accumsan et. Nulla quis dolor rutrum, finibus arcu eget, ornare nisl. Integer ultrices dictum nibh vel sodales. In facilisis dolor eu condimentum congue. Nullam sed maximus nibh. Vivamus interdum dolor sed leo molestie, sed maximus enim tristique. Morbi imperdiet eget massa quis lobortis. Nullam mollis finibus urna, a auctor massa rhoncus ut. Suspendisse orci quam, congue nec nisl at, eleifend porta libero.
+        <div class="content-wrapper">
+          <div class="large-detail image-container"><img :src="image(example, 'large')" /></div>
+          <div class="small-container">
+            <div class="small image-container"><img :src="image(example, '1')" /></div>
+            <div class="small image-container"><img :src="image(example, '2')" /></div>
+            <div class="small image-container"><img :src="image(example, '3')" /></div>
+          </div>
+          <div class="detail-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem at nisi feugiat luctus. Quisque mollis elit lorem, facilisis vehicula leo rutrum non. Quisque ullamcorper pharetra orci, quis venenatis magna dictum sed. In pharetra orci eros, sit amet fringilla dui interdum id. Proin feugiat diam id iaculis pretium. Morbi ac ex eu turpis tincidunt bibendum ullamcorper ut nibh. Nunc vitae consequat massa. Suspendisse vitae est nec mi maximus maximus. Vivamus pretium, orci vitae tristique aliquam, sapien nisl efficitur orci, et ullamcorper sapien mauris id justo. Nulla pulvinar porta nulla sed feugiat. Pellentesque mollis et leo eget viverra. Nunc interdum purus tellus, at porta velit ullamcorper nec. Curabitur porta sem placerat mi tincidunt consequat. Mauris sagittis imperdiet lectus sed mattis. In ut libero et turpis facilisis ultrices a sit amet orci. Nunc pulvinar scelerisque eleifend. Donec feugiat augue sed justo auctor faucibus. Nam ut nisl non est cursus pharetra. Curabitur vestibulum magna eget lectus egestas tempus. Vestibulum massa justo, facilisis id nulla at, eleifend auctor risus. Ut id nibh sem. Phasellus at ullamcorper orci. Cras ac diam massa. Nullam scelerisque ex eu fermentum elementum. Vivamus a urna non tortor molestie vulputate. In nec justo quis urna convallis volutpat. Quisque laoreet eros eu pulvinar posuere. Mauris ut venenatis magna, et commodo nisl. Proin purus nulla, gravida a consequat eget, vestibulum sed enim. Nunc vitae arcu libero. Mauris a tortor felis. Sed sed erat quis lorem congue ornare vitae in dui. In cursus nec magna eget gravida. Maecenas finibus feugiat magna quis lobortis. Suspendisse lacinia quam vel dolor molestie vulputate. Nam ut vulputate massa. Aenean maximus erat odio. Duis lacinia ac est maximus sodales. Curabitur risus risus, commodo nec euismod in, varius ac augue. Vivamus tempor dignissim augue vitae aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eget iaculis lorem, fringilla luctus neque. Nunc quam tortor, vestibulum quis libero ac, consectetur vestibulum tellus. Curabitur imperdiet nisi diam, vitae mattis purus porttitor quis. In sed sem ligula. Vivamus fringilla placerat nisi, in efficitur diam viverra in. Praesent elit quam, consectetur quis est quis, congue mattis lorem. Aenean tempor, eros sit amet semper viverra, tellus quam posuere mauris, eget condimentum nisl elit id quam. Donec sed convallis purus, vel suscipit sem. Proin porta mauris ac eleifend elementum. Proin tellus leo, semper consectetur odio sed, molestie ultricies sem. Vivamus condimentum, purus fringilla mattis euismod, mi lectus mollis metus, at posuere augue libero et erat. Morbi laoreet ultricies nulla, id sodales nisi feugiat vel. Nam sit amet eleifend felis. Aenean eleifend velit sem, et sagittis erat accumsan et. Nulla quis dolor rutrum, finibus arcu eget, ornare nisl. Integer ultrices dictum nibh vel sodales. In facilisis dolor eu condimentum congue. Nullam sed maximus nibh. Vivamus interdum dolor sed leo molestie, sed maximus enim tristique. Morbi imperdiet eget massa quis lobortis. Nullam mollis finibus urna, a auctor massa rhoncus ut. Suspendisse orci quam, congue nec nisl at, eleifend porta libero.
+          </div>
         </div>
       </div>
     </div>
@@ -66,7 +62,15 @@ export default {
         "pomodoro",
         "calculator"
       ],
+      titles: [
+        "Discover MWNF Galleries",
+        "Simon Says",
+        "Tic Tac Toe",
+        "Pomodoro Clock",
+        "JavaScript Calculator"
+      ],
       current: null,
+      hover: null,
       detailsOpen: false,
       position: {
         top: 0,
@@ -74,7 +78,6 @@ export default {
       },
       pin: {
         position: "fixed",
-        // height: null,
         width: null,
         top: 0,
         left: 0
@@ -82,44 +85,38 @@ export default {
       initialTop: null,
     }
   },
-  // watch: {
-  //   detailsOpen: function () {
-  //     if (this.detailsOpen) {
-  //       this.setPositions();
-  //     }
-  //   }
-  // },
   methods: {
+    // Dynamic image URL
     image: function (name, type) {
       return require(`../assets/${name}_${type}.png`);
     },
+    exampleHover: function(event, index) {
+      if (event === "over") {
+        this.hover = index;
+      } else if (event === "leave") {
+        this.hover = null;
+      }
+    },
     getInitialTop: function () {
+      // Get top position and save it
       let element = document.getElementById("examples-container");
       let position = element.getBoundingClientRect();
       this.initialTop = `${position.top}px`;
     },
     setPin: function () {
-      console.log("hello");
-      // let container = document.getElementById("code-container");
-
-      // let container = document.getElementsByClassName("example-detail")[0];
-      // console.log(document.getElementsByClassName("example-detail"));
-      // let size = container.getBoundingClientRect();
-      // this.pin.width = `${size.width}px`;
+      // Set coordinates for "pinning" the container in the background in place
       let examples = document.getElementById("code-container");
       let position = examples.getBoundingClientRect();
-      // this.pin.height = `${position.height}px`
       this.pin.width = `${position.width}px`;
-      console.log(position.top, position.left);
-      // this.pin.top = `${position.top}px`;
       this.pin.top = this.initialTop;
       this.pin.left = `${position.left}px`;
     },
     openDetails: function (index) {
       this.getInitialTop();
+      // Open the appropriate details
       this.current = index;
       this.detailsOpen = true;
-      // this.setPositions();
+      // Wait for elements to load before setting positions
       this.$nextTick(function () {
         this.setPositions();
       });
@@ -152,13 +149,9 @@ export default {
     }
   },
   mounted() {
+    // Responsive sizing
     window.addEventListener("resize", this.setPositions);
   },
-  // updated() {
-  //   if (this.detailsOpen) {
-  //     this.setPositions();
-  //   }
-  // },
   beforeDestroy() {
     window.removeEventListener("resize", this.setPositions);
   }
@@ -201,20 +194,59 @@ export default {
     .large {
       width: 100%;
       position: relative;
-      box-shadow: 0px 0px 10px 0px rgba(128, 128, 128, 0.5);
+      // box-shadow: 0px 0px 10px 0px rgba(128, 128, 128, 0.5);
       // width: 60%;
       // margin-bottom: 10px;
     }
-    .fade::after {
-      content: "";
-      display: block;
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-      box-shadow: inset 0 0 0 200px rgba(white, 0.8);
-    }
+      .large:hover {
+        cursor: pointer;
+      }
+      .large-shadow {
+        box-shadow: 0px 0px 10px 0px rgba(192, 192, 192, 0.5);
+      }
+      body.nightmode .large-shadow {
+        box-shadow: 0px 0px 10px 0px rgba(64, 64, 64, 0.5);
+      }
+      .large-mouseover {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+        .mouseover-text {
+          @include flex-center;
+          height: 100%;
+          width: 100%;
+          color: white;
+          font-size: 130%;
+          font-weight: bold;
+          text-align: center;
+          text-shadow: 0 0 5px black;
+          box-shadow: inset 0 0 0 200px rgba(black, 0.35);
+          padding: 50px;
+          // color: black;
+          // box-shadow: inset 0 0 0 200px rgba(white, 0.5);
+        }
+          // body.nightmode .mouseover-text {
+          //   // color: black;
+          //   // box-shadow: inset 0 0 0 200px rgba(white, 0.5);
+          //   color: white;
+          //   box-shadow: inset 0 0 0 200px rgba(black, 0.5);
+          // }
+      .fade::after {
+        content: "";
+        display: block;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        box-shadow: inset 0 0 0 200px rgba(white, 0.85);
+      }
+      body.nightmode .fade::after {
+        box-shadow: inset 0 0 0 200px rgba(black, 0.9);
+      }
 
 .example-detail {
   @include flex-center;
@@ -233,24 +265,54 @@ export default {
     // top: 0;
     // right: 0;
     font-size: 28px;
+    color: black;
     padding: 10px;
     // background-color: red;
   }
-  .large-detail {
-    width: 60%;
-    box-shadow: 0px 0px 10px 0px rgba(128, 128, 128, 0.5);
-    margin-bottom: 10px;
-  }
-  .small-container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
-    width: 60%;
-  }
-    .small {
-      width: 100%;
-      box-shadow: 0px 0px 10px 0px rgba(128, 128, 128, 0.5);
+    .detail-close:hover {
+      cursor: pointer;
     }
+    body.nightmode .detail-close {
+      color: white;
+    }
+  .content-wrapper {
+    width: 60%;
+  }
+    .large-detail {
+      width: 100%;
+      box-shadow: 0px 0px 10px 0px rgba(192, 192, 192, 0.5);
+      margin-bottom: 10px;
+    }
+      body.nightmode .large-detail {
+        box-shadow: 0px 0px 10px 0px rgba(64, 64, 64, 0.5);
+      }
+    .small-container {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 10px;
+      width: 100%;
+      margin-bottom: 20px;
+    }
+      .small {
+        width: 100%;
+        box-shadow: 0px 0px 10px 0px rgba(192, 192, 192, 0.5);
+      }
+        body.nightmode .small {
+          box-shadow: 0px 0px 10px 0px rgba(64, 64, 64, 0.5);
+        }
+    .detail-text {
+      width: 100%;
+      color: black;
+      background-color: rgba(white, 0.5);
+      box-shadow: 0px 0px 10px 0px rgba(192, 192, 192, 0.5);
+      // border-radius: 20px;
+      padding: 20px;
+    }
+      body.nightmode .detail-text {
+        color: white;
+        background-color: rgba(black, 0.5);
+        box-shadow: 0px 0px 10px 0px rgba(64, 64, 64, 0.5);
+      }
 
 .image-container img {
   @include image-scale;
